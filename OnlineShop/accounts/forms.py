5ef2ce61e6f39850b5a6518d4c089b445f9421fa
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-17 22:03:36 +0330

    feat: add login form

--- OnlineShop/accounts/forms.py
+++ OnlineShop/accounts/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 from .models import User
+from core.utils import get_phonenumber_regex
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -13,3 +14,8 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = User
         fields = ("phone_number",)
+
+
+class LoginForm(forms.Form):
+    phone_number = forms.CharField(label="Phone Number", validators=[get_phonenumber_regex()])
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
