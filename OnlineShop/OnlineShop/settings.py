4018c843db6b03b025a515e216d5fd22feb1696f
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-17 11:00:08 +0330

    feat: postgresql db setting

--- OnlineShop/OnlineShop/settings.py
+++ OnlineShop/OnlineShop/settings.py
@@ -75,11 +75,9 @@ WSGI_APPLICATION = "OnlineShop.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
