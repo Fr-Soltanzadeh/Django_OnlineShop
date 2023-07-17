1e73930d4215a3f624b6d3f7db94f7dd2cd670d7
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-17 17:02:42 +0330

    feat: start app accounts, add user model

---
+++ OnlineShop/accounts/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    phone_number = models.CharField(
+        _("phone number"), max_length=14, unique=True
+    )
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.full_name
+
+    @property
+    def full_name(self):
+        return self.first_name + self.last_name
