f5ffa6664840623f320dbe4db80f20f16d12bb3d
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-18 02:46:28 +0330

    chore: change return of login class view

--- OnlineShop/accounts/views.py
+++ OnlineShop/accounts/views.py
@@ -25,7 +25,7 @@ class LoginView(View):
                 login(request, user)
                 if redirect_to:
                     return HttpResponseRedirect(redirect_to)
-                return render(request, "home.html")
+                return redirect("login")
 
         error_message = "Invalid phone number or password. Please try again."
         return render(
