3d709278b31de0d58a00145a8167b8a0b086d660
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-17 22:10:48 +0330

    feat: login/logout views

--- OnlineShop/accounts/views.py
+++ OnlineShop/accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.views import View
+from django.http import  HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm
 
-# Create your views here.
+
+class LoginView(View):
+    template_name = "accounts/login.html"
+
+    def get(self, request, *args, **kwargs):
+        form = LoginForm()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        redirect_to = request.POST.get("next", "")
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            phone_number = form.cleaned_data["phone_number"]
+            password = form.cleaned_data["password"]
+            user = authenticate(
+                request=request, username=phone_number, password=password
+            )
+            if user:
+                login(request, user)
+                if redirect_to:
+                    return HttpResponseRedirect(redirect_to)
+                return render(request, "home.html")
+
+        error_message = "Invalid phone number or password. Please try again."
+        return render(
+            request, self.template_name, {"form": form, "error_message": error_message}
+        )
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect("login")
