8637ab70b0382b4dfddc00a39d641db9945ec649
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-18 16:00:53 +0330

    feat: otp code model

--- OnlineShop/accounts/models.py
+++ OnlineShop/accounts/models.py
@@ -63,3 +63,16 @@ class Address(BaseModel):
 
     class Meta:
         verbose_name_plural = "Addresses"
+
+
+class OtpCode(models.Model):
+    phone_number = models.CharField(max_length=14, validators=[get_phonenumber_regex()])
+    code = models.PositiveSmallIntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.phone_number} - {self.code} - {self.created_at}"
+
+    class Meta:
+        verbose_name_plural = "OTP Codes"
+
