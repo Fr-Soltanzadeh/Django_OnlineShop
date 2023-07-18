024ee54df53ee2a36b38486e8a8f951135a78712
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-18 10:28:35 +0330

    feat: impl user profile model

--- OnlineShop/accounts/models.py
+++ OnlineShop/accounts/models.py
@@ -3,10 +3,11 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from .managers import UserManager
 from core.utils import get_phonenumber_regex
+from core.models import BaseModel
 
 
 
-class User(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin, BaseModel):
     phone_number = models.CharField(
         _("phone number"), max_length=14, unique=True, validators=[get_phonenumber_regex()]
     )
@@ -14,7 +15,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=50)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
     USERNAME_FIELD = "phone_number"
     REQUIRED_FIELDS = []
     objects = UserManager()
@@ -25,3 +25,17 @@ class User(AbstractBaseUser, PermissionsMixin):
     @property
     def fullname(self):
         return self.first_name + " " + self.last_name
+
+
+class Profile(BaseModel):
+    class GenderChoices(models.IntegerChoices):
+        MALE = 1, "MALE"
+        FEMALE = 2, "FEMALE"
+    gender = models.IntegerField(
+        choices=GenderChoices.choices, default=1, null=True, blank=True
+    )
+    birthday = models.DateField(null=True, blank=True)
+    national_code = models.IntegerField(null=True, blank=True)
+    user = models.OneToOneField("User", on_delete=models.CASCADE, related_name="profile")
+
+
