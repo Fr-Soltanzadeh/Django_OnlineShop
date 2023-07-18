df01fd6d41f82579cbfade2aaaf7faa1e39fc2ac
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-18 12:48:24 +0330

    feat: register product and category

--- OnlineShop/products/admin.py
+++ OnlineShop/products/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Product, Discount, Comment, Category
 
-# Register your models here.
+
+class CategoryInline(admin.TabularInline):
+    model = Category
+    extra = 3
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "price",
+    )
+    search_fields = ("title", "category")
+    autocomplete_fields("category")
+    list_filter = ("category", "is_active")
+    list_per_page = 10
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_filter = ("parent_category",)
+    inlines = [CategoryInline,]
+    list_per_page = 10
