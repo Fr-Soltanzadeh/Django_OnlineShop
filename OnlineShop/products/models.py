326a1765f344b1e10efe7b6194293bfde704b6a6
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-18 11:10:32 +0330

    feat: category model

--- OnlineShop/products/models.py
+++ OnlineShop/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from core.models import BaseModel
 
-# Create your models here.
+
+class Category(ModelInfo):
+    name = models.CharField(max_length=100)
+    parent_category = models.ForeignKey(
+        "Category",
+        on_delete=models.SET_NULL,
+        related_name="categories",
+        null=True,
+        blank=True,
+    )
+    image = models.ImageField(upload_to="images/", default='static/images/category_default.png')
+    slug = models.SlugField()
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
