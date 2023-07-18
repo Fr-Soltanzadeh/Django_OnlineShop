ca079c23a86c0ffbb48db0d52f442deaa1018664
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-18 11:19:21 +0330

    feat: product model

--- OnlineShop/products/models.py
+++ OnlineShop/products/models.py
@@ -11,7 +11,7 @@ class Category(ModelInfo):
         null=True,
         blank=True,
     )
-    image = models.ImageField(upload_to="images/", default='static/images/category_default.png')
+    image = models.ImageField(upload_to="images/", default="static/images/category_default.png")
     slug = models.SlugField()
 
     class Meta:
@@ -20,3 +20,26 @@ class Category(ModelInfo):
     def __str__(self):
         return self.name
 
+
+class Product(BaseModel):
+
+    title = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=2)
+    category = models.ForeignKey(
+        "Category",
+        on_delete=models.SET_NULL,
+        related_name="products",
+        null=True,
+        blank=True,
+    )
+    image = models.ImageField(upload_to="images/", default="static/images/product_default.png")
+    is_active = models.BooleanField(default=True)
+    info = RichTextField()
+    discount = models.ForeignKey("Discount", on_delete=models.SET_DEFAULT, default=0, related_name="products")
+    slug = models.SlugField()
+    
+    class Meta:
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.title
