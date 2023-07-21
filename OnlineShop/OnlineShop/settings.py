9a522b5a346ccb9c2554ba0d3483db2a882f014e
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-21 07:42:12 +0330

    fet: add token signal in signals.py

--- OnlineShop/OnlineShop/settings.py
+++ OnlineShop/OnlineShop/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    'rest_framework.authtoken'
     "ckeditor",
     # apps
     "accounts.apps.AccountsConfig",
