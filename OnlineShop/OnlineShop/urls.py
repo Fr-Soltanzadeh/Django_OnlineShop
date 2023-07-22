490b0cc2f24502bc4afef6cd5eb89a03641b6091
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-22 18:55:11 +0330

    chore: add products_list url

--- OnlineShop/OnlineShop/urls.py
+++ OnlineShop/OnlineShop/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("accounts.urls")),
     path("", include("home.urls")),
+    path("", include("products.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
