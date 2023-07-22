fdc3001e63bfeccdac1b68fcde145068aa85d050
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-22 19:28:17 +0330

    feat: image serialiser

--- OnlineShop/products/serializers.py
+++ OnlineShop/products/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, ProductImage
 
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ("image", "product")
 
 class ProductSerializer(serializers.ModelSerializer):
-    # category =serializers.StringRelatedField(
-    #     many=True,
-    # )
+    images = ProductImageSerializer(many=True)
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = ("title","price","info","discount","images")
