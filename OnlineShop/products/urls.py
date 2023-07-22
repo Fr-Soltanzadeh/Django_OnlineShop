395148c9268e9fccd20fe70304071b813d7aff3e
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-22 19:28:48 +0330

    feat: productview

--- OnlineShop/products/urls.py
+++ OnlineShop/products/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('products/<slug:slug>', views.ProductListView.as_view()),
+    path('product/<slug:slug>', views.ProductDetailView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
