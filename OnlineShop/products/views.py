c2ea24b3ff0d9ae240f2d9425705b6cc03b1c049
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-24 14:47:36 +0330

    feat: product view with render

--- OnlineShop/products/views.py
+++ OnlineShop/products/views.py
@@ -1,18 +1,9 @@
-from .models import Product, Category
-from .serializers import ProductSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.shortcuts import render
+from django.views import View
 
 
-class ProductListApiView(APIView):
+class ProductListView(View):
+    template_name = "product_list_by_category.html"
+    
     def get(self, request, slug):
-        category = Category.objects.get(slug=slug)
-        products = Product.objects.filter(category=category)
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
-
-class ProductDetailApiView(APIView):
-    def get(self, request, slug):
-        product = Product.objects.get(slug=slug)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
+        return render(request, self.template_name, context={"slug": slug})
