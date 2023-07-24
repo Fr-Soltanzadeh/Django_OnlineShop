96468359778eb2fe62c4391592ca7a6a6e1ea82d
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-24 14:45:46 +0330

    feat: product list api view

---
+++ OnlineShop/products/api_views.py
@@ -0,0 +1,18 @@
+from .models import Product, Category
+from .serializers import ProductSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class ProductListApiView(APIView):
+    def get(self, request, slug):
+        category = Category.objects.get(slug=slug)
+        products = Product.objects.filter(category=category)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class ProductDetailApiView(APIView):
+    def get(self, request, slug):
+        product = Product.objects.get(slug=slug)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
