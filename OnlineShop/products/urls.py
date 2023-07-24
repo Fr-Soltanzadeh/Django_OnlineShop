e8f46b103d61532bd387015fbfa7c277b89ebd5c
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-24 14:46:51 +0330

    feat: product urls

--- OnlineShop/products/urls.py
+++ OnlineShop/products/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
+from . import api_views, views
 
 urlpatterns = [
-    path('products/<slug:slug>', views.ProductListApiView.as_view(), name="products"),
-    path('product/<slug:slug>', views.ProductDetailApiView.as_view(), name="product_detail"),
+    path('products/api/<slug:slug>', api_views.ProductListApiView.as_view(), name="products_by_category_api"),
+    path('product/api/<slug:slug>', api_views.ProductDetailApiView.as_view(), name="product_detail_api"),
+    path('products/<slug:slug>', views.ProductListView.as_view(), name="products_by_category"),
+    path('product/<slug:slug>', views.ProductDetailView.as_view(), name="product_detail"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
