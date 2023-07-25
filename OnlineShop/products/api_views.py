ec3bf801f0369018aeb6f334668882d8e31ab4e6
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-26 02:45:48 +0330

    feat: use mixin and genricviews in api views

--- OnlineShop/products/api_views.py
+++ OnlineShop/products/api_views.py
@@ -2,28 +2,50 @@ from .models import Product, Category
 from .serializers import ProductSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import mixins
 
 
-class ProductListByCategoryApiView(APIView):
-    def get(self, request, slug):
-        category = Category.objects.get(slug=slug)
-        products = Product.objects.filter(category=category)
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+class ProductListByCategoryApiView(mixins.ListModelMixin, generics.GenericAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    def get(self, request, *args, **kwargs):
+        category = Category.objects.get(slug=kwargs['slug'])
+        self.queryset = self.queryset.filter(category=category)   
+        return self.list(request, *args, **kwargs)
 
-class ProductListApiView(APIView):
 
-    def get(self, request):
+class ProductListApiView(mixins.ListModelMixin, generics.GenericAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    def get(self, request, *args, **kwargs):
         search_phrase = request.GET.get("search")
-        if not search_phrase:
-            products = Product.objects.all()
-        else:
-            products = Product.objects.filter(title__icontains = search_phrase)
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+        if search_phrase:
+            self.queryset = self.queryset.filter(title__icontains = search_phrase)    
+        return self.list(request, *args, **kwargs)
 
-class ProductDetailApiView(APIView):
-    def get(self, request, slug):
-        product = Product.objects.get(slug=slug)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
+
+class ProductDetailApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field= "slug"
+
+
+
+# class ProductListByCategoryApiView(APIView):
+#     def get(self, request, slug, format=None):
+#         category = Category.objects.get(slug=slug)
+#         products = Product.objects.filter(category=category)
+#         serializer = ProductSerializer(products, many=True)
+#         return Response(serializer.data)
+
+
+# class ProductListApiView(APIView):
+#     def get(self, request, format=None):
+#         search_phrase = request.GET.get("search")
+#         if not search_phrase:
+#             products = Product.objects.all()
+#         else:
+#             products = Product.objects.filter(title__icontains = search_phrase)
+#         serializer = ProductSerializer(products, many=True)
+#         return Response(serializer.data)
