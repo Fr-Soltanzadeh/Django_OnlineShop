18e961291c4939174d77a827e2e7af81a7ac37eb
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-27 03:58:17 +0330

    feat: cart html

--- OnlineShop/cart/views.py
+++ OnlineShop/cart/views.py
@@ -5,4 +5,4 @@ from django.views import View
 class CartView(View):
     template_name="cart.html"
     def get(self, request):
-        render(request, template_name)
+        return render(request, self.template_name)
