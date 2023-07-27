ae9e52803eb55d0b0b0e072cd18bd734d0b427b5
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-27 04:45:37 +0330

    chore: change template folders for cleanness

--- OnlineShop/cart/views.py
+++ OnlineShop/cart/views.py
@@ -3,6 +3,6 @@ from django.views import View
 
 
 class CartView(View):
-    template_name="cart.html"
+    template_name="cart/cart.html"
     def get(self, request):
         return render(request, self.template_name)
