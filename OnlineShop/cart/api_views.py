60903e5b858bfd20283f39518a8557bb11f3ca69
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-29 17:20:05 +0330

    feat: apiview for put and delete cart data

--- OnlineShop/cart/api_views.py
+++ OnlineShop/cart/api_views.py
@@ -13,7 +13,6 @@ class CartApiView(APIView):
             cart = user.cart
          except:
             cart = Cart.objects.create(customer=user)
-         
       else:
          try:
             cart = request.session.get('cart')
@@ -22,3 +21,45 @@ class CartApiView(APIView):
             request.session["cart"] = cart
       serializer = CartSerializer(cart)
       return Response(serializer.data)
+
+   def delete(self, request):
+      if request.user.is_authenticated:
+         user = request.user
+         user.cart.cart_items.all().delete()
+         serializer = CartSerializer(user.cart)
+      else:
+         try:
+            request.session['cart']["cart_items"]=[]
+         except KeyError:
+            cart = {"customer":None,"cart_items":[]}
+            request.session["cart"] = cart
+         serializer = CartSerializer(request.session["cart"])
+      return Response(serializer.data)
+
+   def put(self, request):
+      cart_items = request.data.get('cart_items')
+      cart_items={item['product_id']:item['quantity'] for item in cart_items}
+      print(cart_items)
+      if not cart_items:
+         return Response({'error': 'No cart items provided'}, status=status.HTTP_400_BAD_REQUEST)
+      if request.user.is_authenticated:
+         user = request.user
+         user_cart_items=user.cart.cart_items.all()
+         print(cart_items.keys())
+         for item in user_cart_items:
+           
+            if str(item.product.id) not in cart_items.keys():
+               item.delete()
+            else:
+               item.quantity=cart_items[str(item.product.id)]
+               item.save()
+         serializer = CartSerializer(user.cart)
+
+      else:
+         try:
+            cart = request.session.get('cart')
+         except:
+            cart = {"customer":None,"cart_items":[]}
+            request.session["cart"] = cart
+         serializer = CartSerializer(cart)
+      return Response(serializer.data) 
