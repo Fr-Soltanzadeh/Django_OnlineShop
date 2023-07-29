9bfcbeabca12870ca7cc3548fa4150fe3f321bfc
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-29 17:19:23 +0330

    feat: signal for create cart instance

--- OnlineShop/accounts/signals.py
+++ OnlineShop/accounts/signals.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 from .models import CustomerProfile, Customer
+from cart.models import Cart
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
@@ -12,6 +13,12 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
 
 
 @receiver(post_save, sender=Customer)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
+def create_customer_profile(sender, instance=None, created=False, **kwargs):
     if created:
         CustomerProfile.objects.create(customer=instance)
+
+
+@receiver(post_save, sender=User)
+def create_cart(sender, instance=None, created=False, **kwargs):
+    if created and instance.role==1:
+        Cart.objects.create(customer=instance)
