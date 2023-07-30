6f7573033b486c4bffa9647e8c48deb38f75a081
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-30 20:23:58 +0330

    fix: change code to work for un auth user

--- OnlineShop/cart/api_views.py
+++ OnlineShop/cart/api_views.py
@@ -1,8 +1,13 @@
 from .models import CartItem, Cart
+from products.models import Product
+from products.serializers import ProductSerializer
 from accounts.models import Customer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import CartItemSerializer, CartSerializer
+from django.db import IntegrityError
+from decimal import Decimal
+from .utils import calculate_grand_price
 
 
 class CartApiView(APIView):
@@ -13,27 +18,34 @@ class CartApiView(APIView):
             cart = user.cart
          except:
             cart = Cart.objects.create(customer=user)
+         serializer = CartSerializer(user.cart)
+         return Response(serializer.data)
       else:
          try:
+            print(request.session['cart'])
             cart = request.session.get('cart')
          except:
             cart = {"customer":None,"cart_items":[], "grand_price":0, "total_price":0}
             request.session["cart"] = cart
-      serializer = CartSerializer(cart)
-      return Response(serializer.data)
+      return Response(cart)
+
 
    def delete(self, request):
       if request.user.is_authenticated:
          user = request.user
          user.cart.cart_items.all().delete()
          serializer = CartSerializer(user.cart)
+         return Response(serializer.data)
       else:
          try:
             request.session['cart']["cart_items"]=[]
+            request.session['cart']["grand_price"]=0
+            request.session['cart']["total_price"]=0
          except KeyError:
             request.session["cart"] = {"customer":None,"cart_items":[], "grand_price":0, "total_price":0}
-         serializer = CartSerializer(request.session["cart"])
-      return Response(serializer.data)
+         print(request.session['cart'])
+         request.session.save()
+      return Response(request.session["cart"])
 
    def put(self, request):
       cart_items = request.data.get('cart_items')
@@ -48,42 +60,52 @@ class CartApiView(APIView):
                item.quantity=cart_items[str(item.product.id)]
                item.save()
          serializer = CartSerializer(user.cart)
+         return Response(serializer.data) 
       else:
          try:
             user_cart_items = request.session.get('cart')['cart_items']
-            for i in range(len(user_cart_items)):
-               item=user_cart_items[i]
+            for index,item in enumerate(user_cart_items):
                if str(item['product']["id"]) not in cart_items.keys():
-                  request.session["cart"]['cart_items'].pop(i)
+                  request.session["cart"]['cart_items'].pop(index)
                else:
-                  request.session["cart"]['cart_items'][i]['quantity']=cart_items[str(item['product']['id'])]
+                  request.session["cart"]['cart_items'][index]['quantity']=cart_items[str(item['product']['id'])]
          except:
             request.session["cart"] = {"customer":None,"cart_items":[], "grand_price":0, "total_price":0}
-         serializer = CartSerializer(request.session["cart"])
-      return Response(serializer.data) 
+         request.session.save()
+         calculate_grand_price(request)
+         return Response(request.session["cart"])
+      
+
+class AddToCartApiView(APIView):
       def post(self, request):
-         cart_items = request.data.get('cart_items')
-         cart_items={item['product_id']:item['quantity'] for item in cart_items}
+         product_id=request.data.get('product_id')
+         product = Product.objects.get(id=int(product_id))
          if request.user.is_authenticated:
             user = request.user
-            user_cart_items=user.cart.cart_items.all()
-            for item in user_cart_items:
-               if str(item.product.id) not in cart_items.keys():
-                  item.delete()
-               else:
-                  item.quantity=cart_items[str(item.product.id)]
-                  item.save()
-            serializer = CartSerializer(user.cart)
-         else:
             try:
-               user_cart_items = request.session.get('cart')['cart_items']
-               for i in range(len(user_cart_items)):
-                  item=user_cart_items[i]
-                  if str(item['product']["id"]) not in cart_items.keys():
-                     request.session["cart"]['cart_items'].pop(i)
-                  else:
-                     request.session["cart"]['cart_items'][i]['quantity']=cart_items[str(item['product']['id'])]
-            except:
-               request.session["cart"] = {"customer":None,"cart_items":[], "grand_price":0, "total_price":0}
-            serializer = CartSerializer(request.session["cart"])
-         return Response(serializer.data) 
+               cart_item = CartItem.objects.create(product=product,cart = user.cart)
+               cart_item.quantity=1
+               cart_item.save()
+            except IntegrityError:
+               cart_item=CartItem.objects.get(product=product , cart=user.cart)
+               cart_item.quantity+=1
+               cart_item.save()
+            serializer = CartSerializer(user.cart)
+            return Response(serializer.data)
+         else:
+            product=ProductSerializer(product).data 
+            product['price']=str(product['price'])
+            product['discounted_price']=str(product['discounted_price'])
+            try:
+               for index,x  in enumerate(request.session["cart"]['cart_items']):
+                  print(index, x)
+               item_index=[index for index,x  in enumerate(request.session["cart"]['cart_items']) if x['product']['id']==product_id][0]
+               request.session["cart"]['cart_items'][item_index]['quantity']+=1
+            except IndexError:
+               if not request.session.get("cart"):
+                  request.session["cart"] = {"customer":None,"cart_items":[], "grand_price":0, "total_price":0}
+               request.session["cart"]['cart_items'].append({'quantity':1,'product':product})
+            print(request.session["cart"])
+            request.session.save()
+            calculate_grand_price(request)
+            return Response(request.session["cart"])
