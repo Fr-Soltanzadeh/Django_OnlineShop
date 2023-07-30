060a16cf510d1b3da7bdab3981294eed6efdb446
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-30 20:29:01 +0330

    tet: test url cart api

--- OnlineShop/cart/test/test_urls.py
+++ OnlineShop/cart/test/test_urls.py
@@ -1,9 +1,18 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from ..views import CartView
+from ..api_views import CartApiView, AddToCartApiView
 
 
 class TestUrls(SimpleTestCase):
-    def test_products(self):
+    def test_cart(self):
         url = reverse("cart")
         self.assertEqual(resolve(url).func.view_class, CartView)
+
+    def test_add_to_cart_api(self):
+        url = reverse("cart_api")
+        self.assertEqual(resolve(url).func.view_class, CartApiView)
+
+    def test_add_to_cart_api(self):
+        url = reverse("add_to_cart_api")
+        self.assertEqual(resolve(url).func.view_class, AddToCartApiView)
