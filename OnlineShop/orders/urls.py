60218f056a32eea3591399a42f6f5a7a9e3b28cb
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-08-06 12:42:27 +0330

    feat: add views for zarinpal

--- OnlineShop/orders/urls.py
+++ OnlineShop/orders/urls.py
@@ -5,6 +5,8 @@ from . import api_views, views
 urlpatterns = [
     # path("checkout/api/v1/", api_views.CheckoutApiView.as_view(), name="checkout_api"),
     path("checkout/", views.CheckoutView.as_view(), name="checkout"),
+    path("pay/<int:order_id>/", views.OrderPayView.as_view(), name="pay"),
+    path("verify_order/", views.VerifyOrderView.as_view(), name="verufy_order"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
