4ae219e00d15e3084929351c437ccd41acb746b1
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-08-07 16:55:23 +0330

    feat: add order serializer

---
+++ OnlineShop/orders/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Order, OrderItem
+from accounts.serializers import CustomerSerializer
+
+
+class OrderItemSerilizer(serializers.ModelSerializer):
+    product = serializers.SerializerMethodField()
+
+    class Meta:
+        model = OrderItem
+        fields = ("price", "quantity","product")
+
+    def get_product(self,order_item):
+        return order_item.product.title
+
+class OrderSerializer(serializers.ModelSerializer):
+    orderItems = OrderItemSerilizer(many=True, read_only=True)
+    customer = CustomerSerializer(read_only=True)
+    class Meta:
+        model = Order
+        fields="__all__"
