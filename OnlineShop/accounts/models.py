7f573044ec4d1b7d4882e740150fe8080b0b47ed
FSoltanzadeh <f.soltanzadeh1378@gmail.com>
2023-08-27 02:47:35 +0330

    chore: change shaba-number to integerfield

--- OnlineShop/accounts/models.py
+++ OnlineShop/accounts/models.py
@@ -59,7 +59,7 @@ class CustomerProfile(BaseModel):
     customer = models.OneToOneField(
         "Customer", on_delete=models.CASCADE, related_name="profile"
     )
-    shaba_number = models.CharField(max_length=26, null=True, blank=True)
+    shaba_number = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
         return str(self.customer)
